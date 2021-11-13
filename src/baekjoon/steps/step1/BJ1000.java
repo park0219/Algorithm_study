fd8fcf64d6e12c41030e8eef5be696cc4043a2f7
Park SungHwan <park0219ok@outlook.com>
2021-11-13 13:56:45 +0900

    feat: 백준 1000번 완료 https://www.acmicpc.net/problem/1000

---
+++ src/baekjoon/steps/step1/BJ1000.java
@@ -0,0 +1,16 @@
+package baekjoon.steps.step1;
+
+import java.util.Scanner;
+
+public class BJ1000 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.print(A + B);
+        sc.close();
+    }
+}
