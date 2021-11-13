4d7210128bae208dffa36a305906d7b56fcbde45
Park SungHwan <park0219ok@outlook.com>
2021-11-13 13:26:07 +0900

    feat: 백준 10718번 완료 https://www.acmicpc.net/problem/10718

---
+++ src/baekjoon/steps/step1/BJ10718.java
@@ -0,0 +1,9 @@
+package baekjoon.steps.step1;
+
+public class BJ10718 {
+
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+    }
+}
