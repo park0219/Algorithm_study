99f2a4fcb4780fbb8e518d7ca87243bfd8ebc0c2
Park SungHwan <park0219ok@outlook.com>
2021-11-13 13:45:29 +0900

    feat: 백준 10171번 완료 https://www.acmicpc.net/problem/10171

---
+++ src/baekjoon/steps/step1/BJ10171.java
@@ -0,0 +1,11 @@
+package baekjoon.steps.step1;
+
+public class BJ10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
