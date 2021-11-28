fc21cef1de479d07755e03297c23751946aad0aa
Park SungHwan <park0219ok@outlook.com>
2021-11-28 16:03:10 +0900

    feat: 백준 10952번 완료 https://www.acmicpc.net/problem/10952

---
+++ src/baekjoon/steps/step4/BJ10952.java
@@ -0,0 +1,26 @@
+package baekjoon.steps.step4;
+
+import java.util.Scanner;
+
+public class BJ10952 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if(A == 0 && B == 0) {
+                break;
+            }
+
+            System.out.println(A + B);
+        }
+
+        sc.close();
+
+    }
+}
