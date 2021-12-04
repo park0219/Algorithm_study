26f6880c2c732b81933dbd6261f0fca5b5a3aeca
Park SungHwan <park0219ok@outlook.com>
2021-12-04 22:15:13 +0900

    feat: 백준 3052번 완료 https://www.acmicpc.net/problem/3052

---
+++ src/baekjoon/steps/BJ3052.java
@@ -0,0 +1,22 @@
+package baekjoon.steps;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BJ3052 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for(int i = 0; i < 10; i++) {
+            hashSet.add(sc.nextInt() % 42);
+        }
+
+        System.out.println(hashSet.size());
+
+        sc.close();
+    }
+}
