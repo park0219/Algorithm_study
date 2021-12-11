0f85ca98076d5b5c6af50708bcd659e81a598b47
Park SungHwan <park0219ok@outlook.com>
2021-12-11 22:24:07 +0900

    feat: 백준 11654번 완료 https://www.acmicpc.net/problem/11654

---
+++ src/baekjoon/steps/step7/BJ11654.java
@@ -0,0 +1,18 @@
+package baekjoon.steps.step7;
+
+import java.util.Scanner;
+
+public class BJ11654 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.next().charAt(0);
+
+        System.out.println(input);
+
+        sc.close();
+
+    }
+}
