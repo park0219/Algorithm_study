9e0d57568d2600ef84661af6ae7337493afbe6b5
Park SungHwan <park0219ok@outlook.com>
2021-12-21 08:47:19 +0900

    feat: 백준 1712번 완료 https://www.acmicpc.net/problem/1712

---
+++ src/baekjoon/steps/step8/BJ1712.java
@@ -0,0 +1,25 @@
+package baekjoon.steps.step8;
+
+import java.util.Scanner;
+
+public class BJ1712 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if(B >= C) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println((A / (C - B) + 1));
+        }
+
+        sc.close();
+
+    }
+}
