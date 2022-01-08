26823b686a68a530a52a588b3cfd9ee703052291
Park SungHwan <park0219ok@outlook.com>
2022-01-08 17:36:17 +0900

    feat: 백준 4153번 완료 https://www.acmicpc.net/problem/4153

---
+++ src/baekjoon/steps/step9/BJ4153.java
@@ -0,0 +1,39 @@
+package baekjoon.steps.step9;
+
+import java.util.Scanner;
+
+public class BJ4153 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            if(a * a == (b * b + c * c)) {
+                System.out.println("right");
+            }
+            else if(b * b == (a * a + c * c)) {
+                System.out.println("right");
+            }
+            else if(c * c == (a * a + b * b)) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
+
+        }
+
+        sc.close();
+
+    }
+}
