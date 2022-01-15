66e3c30a79202cf33195536d5c2cf5e4c6b0277c
Park SungHwan <park0219ok@outlook.com>
2022-01-16 00:38:19 +0900

    feat: 백준 2231번 완료 https://www.acmicpc.net/problem/2231

---
+++ src/baekjoon/steps/step11/BJ2231.java
@@ -0,0 +1,34 @@
+package baekjoon.steps.step11;
+
+import java.util.Scanner;
+
+public class BJ2231 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while(number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+
+    }
+}
