3529d9a0cd6238f234e2e44b6ff0228a6c96c3eb
Park SungHwan <park0219ok@outlook.com>
2022-01-22 14:07:11 +0900

    feat: 백준 10989번 완료 https://www.acmicpc.net/problem/10989

---
+++ src/baekjoon/steps/step12/BJ10989.java
@@ -0,0 +1,34 @@
+package baekjoon.steps.step12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10989 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < 10001; i++) {
+            while(arr[i] > 0) {
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
+        }
+
+        System.out.println(sb);
+
+        br.close();
+
+    }
+}
